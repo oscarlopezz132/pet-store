682f8ca0dba74d2d87f8e091096de192e9eeb01c
oscarlopezz132 <oscarlopezz132@gmail.com>
2025-04-03 20:11:26 -0500

    validacion correo registro usuario

--- src/signup.php
+++ src/signup.php
@@ -7,17 +7,39 @@ $lname = $_POST ['l_name'];
 $email = $_POST ['e_mail'];
 $passw = $_POST ['p_assw'];
 
-
-$sql = "INSERT INTO users 
-
-(firstname, lastname, email, password)
-VALUES ('$fname','$lname','$email','$passw')
+$sql_validate_email= "
+select
+count(id) as total
+from
+users
+where
+email='$email' and
+status =true
 ";
-$ans = pg_query($conn, $sql);
+$ans = pg_query($conn, $sql_validate_email);
+
 if($ans){
-    echo "User creado";
-}else{
-    echo "error";
+    $row = pg_fetch_assoc($ans);
+    if($row['total']>0){
+        echo "User already exits !!!" ;
+     }else{
+        $sql = "INSERT INTO users 
+        (firstname, lastname, email, password)
+        VALUES ('$fname','$lname','$email','$passw')
+        ";
+        $ans = pg_query($conn, $sql);
+        if($ans){
+            echo "User creado";
+        }else{
+            echo "error";
+        }
+   
 }
+}else{
+
+   echo "Query error";
+}
+
+
 
 ?>
